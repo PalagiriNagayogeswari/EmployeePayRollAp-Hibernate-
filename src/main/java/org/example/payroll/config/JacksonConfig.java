83ede969d720570ee17fdb7353866ef00b41200b
PalagiriNagayogeswari <yogi34343@gmail.com>
2026-08-19 00:19:47 +0530

    [Nagayogeswari]:Added Jackson configuration file to configure date module.

---
+++ src/main/java/org/example/payroll/config/JacksonConfig.java
@@ -0,0 +1,25 @@
+package org.example.payroll.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+//used for configuration of time module.
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+
+        ObjectMapper objectMapper =
+                new ObjectMapper();
+
+        objectMapper.registerModule(
+                new JavaTimeModule()
+        );
+
+        return objectMapper;
+    }
+}
